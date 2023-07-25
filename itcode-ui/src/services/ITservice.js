627c0ee561628bf3ae09c7b532737cbe771ec00b
Shidhartha <sidroy192@gmail.com>
2023-07-25 13:33:04 +0530

    Changed api path

--- itcode-ui/src/services/ITservice.js
+++ itcode-ui/src/services/ITservice.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const TAX_CODE_API_BASE_URL = "http://localhost:8080/api/v1/result";
+const TAX_CODE_API_BASE_URL = "http://localhost:8080/aaykarsaathi/result";
 
 class ITservice {
 
