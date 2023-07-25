b8a4e2f15073214972ce6b72709caf295ab6b0c8
Shidhartha <sidroy192@gmail.com>
2023-07-25 10:29:53 +0530

    Home animation

--- itcode-ui/src/components/Home.js
+++ itcode-ui/src/components/Home.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import landing from "../images/landing-image.png"
+import { motion } from "framer-motion";
+import { useInView } from 'react-intersection-observer'
+
 
 const Home = () => {
 
   const navigate = useNavigate();
+  const [ref, inView] = useInView({
+    threshold: 0,
+    triggerOnce: true,
+  });
 
   const tryService = (e) => {
     e.preventDefault();
@@ -15,7 +22,12 @@ const Home = () => {
   return (
     <div className="h-screen flex flex-col">
     
-      <div className="text-green-200 font-extrabold font-mono tracking tracking-wider mt-40 max-w-xl" style={{textAlign: "left"}}>
+      <motion.div 
+      ref={ref}
+      initial={{ x: "-10vw", opacity: 0 }} // Off-screen position to the left
+      animate={inView ? { x: 0, opacity: 1 } : { x: "-10vw", opacity: 0 }} // Animate to the original position (left to right)
+      transition={{ duration: 0.6, ease: "easeInOut" }} // Set the transition type and duration
+      className="text-green-200 font-extrabold font-mono tracking tracking-wider mt-40 max-w-xl" style={{textAlign: "left"}}>
       
         <div className="text-6xl pl-5 text-green-400" >
      WELCCOME TO, AAYKAR SAATHI:
@@ -28,10 +40,16 @@ const Home = () => {
       TRY
       </div> 
       </div>
-      </div>
-      <div className="flex justify-end mr-10 -mt-80">
+      </motion.div>
+      <motion.div
+      ref={ref}
+      initial={{ x: "10vw", opacity: 0 }}
+      animate={inView ? { x: 0, opacity: 1 } : { x: "10vw", opacity: 0 }}
+      transition={{ duration: 0.6, ease: "easeInOut" }}
+      
+      className="flex justify-end mr-10 -mt-80">
         <img src={landing} className="max-w-xl" alt='Landing Page'/>
-      </div>
+      </motion.div>
       <div className="pl-5">
       <button onClick={tryService} className="h-9 pb-3 pl-3 pr-3 pt-1 -my-24 flex justify-start  tracking-wider text-xl text-white hover:text-white  font-bold transition-colors duration-150 bg-red-800 rounded-xl hover:bg-green-500 ">AAYKAR SAATHI</button>
       </div>
