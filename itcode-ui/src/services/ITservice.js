9d8594b64b0556072d6ec524d1b42643171a4ad3
Shidhartha <sidroy192@gmail.com>
2023-07-27 22:51:14 +0530

    saving destructive changes

--- itcode-ui/src/services/ITservice.js
+++ itcode-ui/src/services/ITservice.js
@@ -2,11 +2,21 @@ import axios from "axios";
 
 const TAX_CODE_API_BASE_URL = "http://localhost:8080/aaykarsaathi/result";
 
+const USERNAME = "jack";
+const PASSWORD = "shukla";
+
+
 class ITservice {
 
     getCodeDataById(id){
+
+        const config = {
+            headers: {
+                Authorization: "Basic " + btoa(`${USERNAME}:${PASSWORD}`),
+            },
+        };
         
-        return axios.get(TAX_CODE_API_BASE_URL + "/" + id);
+        return axios.get(TAX_CODE_API_BASE_URL + "/" + id, config);
         
     }
 
