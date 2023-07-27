03f403be985dddce74bbf99d60cdc68942096ef8
Shidhartha <sidroy192@gmail.com>
2023-07-27 08:55:06 +0530

    Api unecessary changes

--- it-code-api/src/main/java/com/sid/itcodeapi/ItCodeApiApplication.java
+++ it-code-api/src/main/java/com/sid/itcodeapi/ItCodeApiApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ItCodeApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ItCodeApiApplication.class, args);
-	}
+	public static void main(String[] args) { SpringApplication.run(ItCodeApiApplication.class, args); }
 
 }
