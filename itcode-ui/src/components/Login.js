e96cb504e3d12fafe2c523d7cae3fd1ef603581e
Shidhartha <sidroy192@gmail.com>
2023-07-27 20:39:16 +0530

    login services file

---
+++ itcode-ui/src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className="h-screen flex flex-col text-white items-center justify-center">
+      
+    </div>
+  )
+}
+
+export default Login
---
+++ itcode-ui/src/components/Services.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div className="h-screen flex flex-col text-white items-center justify-center">
+      
+    </div>
+  )
+}
+
+export default Services
